10be90b791b7a800dd68afd137404bb5ced76102
Jessica Cousins <113946660+jessicacousins@users.noreply.github.com>
2024-01-11 23:30:11 -0500

    logic for add and remove

--- app.js
+++ app.js
@@ -13,4 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
           pizza.id === currentPizza ? "rotateY(0deg)" : "rotateY(180deg)";
       });
     });
+
+  const updateQuantity = (element, delta) => {
+    let quantitySpan = element.parentNode.querySelector(".quantity");
+    let currentQuantity = parseInt(quantitySpan.textContent);
+    let newQuantity = currentQuantity + delta;
+    if (newQuantity >= 0) {
+      quantitySpan.textContent = newQuantity;
+    }
+  };
+
+  document.querySelectorAll(".plus").forEach((button) => {
+    button.addEventListener("click", () => updateQuantity(button, 1));
+  });
+
+  document.querySelectorAll(".minus").forEach((button) => {
+    button.addEventListener("click", () => updateQuantity(button, -1));
+  });
 });
